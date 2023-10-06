abf32bae72b60febbf9811d2fce3610951203fdf
Otrackk <matteomontenero15@gmail.com>
2023-10-06 13:38:35 +0200

    c

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, HashRouter, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
         <React.StrictMode>
-                <BrowserRouter>
+                <HashRouter>
                         <Routes>
                                 <Route path='/*' element={<App />} />
                         </Routes>
-                </BrowserRouter>
+                </HashRouter>
         </React.StrictMode>
 );
