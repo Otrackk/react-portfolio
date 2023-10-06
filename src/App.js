3e22ba813ba68cdcbe60b65a0f46ce0ab387ceac
Otrackk <matteomontenero15@gmail.com>
2023-10-06 10:32:40 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import './style/style.css';
 import Projects from "./pages/Projects";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
 
-    <BrowserRouter>
-      <AnimatePresence>
-        <Routes>
+    
+      <BrowserRouter>
+        <AnimatePresence>
+          <Routes>
 
-          <Route index path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/projects" element={<Projects />}></Route>
+            <Route index path="/react-portfolio" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
 
-        </Routes>
-      </AnimatePresence>
-    </BrowserRouter>
+          </Routes>
+        </AnimatePresence>
+      </BrowserRouter>
+    
   );
 }
 
