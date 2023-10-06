992ab6253c89b0d8d9bff0c308c40ff74dab14ff
Otrackk <matteomontenero15@gmail.com>
2023-10-06 10:29:13 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './style/style.css';
 import Projects from "./pages/Projects";
 import Home from "./pages/Home";
