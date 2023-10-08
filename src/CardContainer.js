f6316f653a0c1882beab19f4193586c74c412889
Otrackk <matteomontenero15@gmail.com>
2023-10-08 16:03:37 +0200

    a

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -4,7 +4,7 @@ import SkillNaming from "./SkillNaming";
 
 const CardContainer = () => {
     return (
-        <div className="container-fluid d-flex justify-content-center row p-5 py-0">
+        <div id="card-container" className="container-fluid d-flex justify-content-center row p-5 py-0">
             <SkillCard />
             <SkillNaming />
         </div>
