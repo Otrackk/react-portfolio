113d43303defd4c4708e2eaff09f3d94714b91c2
Otrackk <matteomontenero15@gmail.com>
2023-10-26 19:46:41 +0200

    final-commit

--- src/Bio.js
+++ src/Bio.js
@@ -1,11 +1,8 @@
 import React from "react";
 
-
 const Bio = () => {
     return (
-
         <div id="my-bio" className="text-light text-start container d-flex flex-column justify-content-start p-5 m-3 pt-0 mt-0">
-
             <p>My name is Matteo Montenero, i am an italian <span className="text-info">Front-End Developer & Algorithms Programmer</span> experimenting with the web as a creative medium.<br /> <br /> This portfolio changes each year in pursuit of learning and using web technology in unexpected ways.
 
                 <br /> <br /> Right now I work as a Freelance Developer & Algorithms Programmer.
@@ -13,10 +10,7 @@ const Bio = () => {
                 <br /> <br /> I specialize in manipulating the very fabric of the web, its raw materials of HTML, CSS, JavaScript and working with React & Vue frameworks.
             </p>
         </div>
-
-
     )
 }
-
 
 export default Bio;
